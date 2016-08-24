4f326fd0a436813446a640b2fa78825b67dda68a
cloudersjun <cloudersjun@gmail.com>
2016-08-24 13:36:21 +0800

    Update App.java
    增加断点续传的支持，同时在统计时如果是当前日期，则可以不用输入参数，也可以输入指定

--- App.java
+++ App.java
@@ -1,11 +1,16 @@
 package db.statistics;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+
+import org.apache.commons.lang3.time.DateFormatUtils;
+import org.apache.commons.lang3.time.DateUtils;
 
 import dao.CidListDao;
 import dao.Dao;
@@ -38,32 +43,42 @@ public class App {
 			t = new Thread(this);
 			// System.out.println("Child thread: " + t);
 			t.start();
+
 		}
 
 		public void run() {
 			try {
 				db_statistics(dbno, dbgroupid, starttime, endtime);
-				Dao.getDao().close(1);
 				Thread.sleep(1000);
 			} catch (InterruptedException e) {
 				System.out.println(e);
 			}
-
+			Dao.getDao().close(1);
 		}
 	}
 
 	static int db[][] = { { 2, 3, 4, 5, 7 }, { 2, 1, 3, 4, 5 } };
 
-	// static int db[][] = { { 2, 7 }, { 2, 5 } };
+	// static int db[][] = { { 7 }, { 5 } };
 
 	public static String driver = "com.mysql.jdbc.Driver";
 
 	public static void main(String[] args) {
 		for (int i = 0; i < db[0].length; i++) {
 			System.out.println("db" + db[0][i] + " begin: ");
-			new NewThread(db[0][i], db[1][i], args[0], args[1]);
+			String starttime = "";
+			String endtime = "";
+			// System.out.println(args.length);
+			if (args.length == 0) {
+				starttime = DateFormatUtils.format(new Date(), "yyyy-MM-dd");
+				endtime = DateFormatUtils.format(DateUtils.addDays(new Date(), +1), "yyyy-MM-dd");
+			} else if (args.length == 2) {
+				starttime = args[0];
+				endtime = args[1];
+			}
+			// System.out.println(starttime + " " + endtime);
+			new NewThread(db[0][i], db[1][i], starttime, endtime);
 			// thread.run();
-
 		}
 		System.out.println(new Date().toString());
 	}
@@ -71,47 +86,69 @@ public class App {
 	public static void db_statistics(int dbno, int dbgroupid, String starttime, String endtime) {
 		List<Integer> cids = new ArrayList<>();
 		CidListDao cidListDao = new CidListDao();
+		cids = cidListDao.getAllCidByStartTimeEndTimeDbGroupid(starttime, endtime, dbgroupid);
+		System.out.println("db:" + dbgroupid + "\tcid size:" + cids.size());
 		try {
 			Thread.sleep(1000);
 		} catch (InterruptedException e1) {
 			e1.printStackTrace();
 		}
-		cids = cidListDao.getAllCidByStartTimeEndTimeDbGroupid(starttime, endtime, dbgroupid);
-//		Dao.getDao().close(dbno);
-		SizeDao sizeDao = new SizeDao();
 		String directory = "E:\\" + starttime;
 		String filename = directory + "\\DB" + dbno + ".txt";
+		// String tempname = directory + "\\DB" + dbno + ".temp";
 		File file = new File(filename);
 		File dir = new File(directory);
 		FileOutputStream out = null;
+		BufferedReader bf = null;
+		String head = "cid\tsize\tkwcount\n";
+		String result = head;
+		List<Integer> alreadyCids = new ArrayList<>();
 		try {
 			if (!dir.exists()) {
 				dir.mkdirs();
 			}
-			file.createNewFile();
+			if (file.exists()) {
+				bf = new BufferedReader(new FileReader(file));
+			} else {
+				file.createNewFile();
+			}
+			out = new FileOutputStream(file, true);
+			if (bf != null) {
+				String line = "";
+				while ((line = bf.readLine()) != null) {
+					String[] lines = line.split("\t");
+					if (lines.length == 3 && !lines[2].equals("null")) {
+						if (!lines[0].equals("cid")) {
+							int cid = Integer.valueOf(lines[0]);
+							System.out.println("don't statistic cid: " + cid);
+							alreadyCids.add(cid);
+						}
+					} else {
+						System.out.print(line);
+						result += line;
+					}
+				}
+			} else {
+				out.write(head.getBytes());
+			}
+			cids.removeAll(alreadyCids);
+			double count = -1;
+			String size = "-1";
+			// String oldsize = "-1";
+			SizeDao sizeDao = new SizeDao();
+			for (Integer cid : cids) {
+				size = sizeDao.getSizeByCid(dbno, cid);
+				// oldsize = sizeDao.getOldSizeByCid(dbno, cid);
+				count = sizeDao.getKwCountByCidStartTime(dbno, cid, starttime);
+				String s = cid + "\t" + size + "\t" + count + "\n";
+				out.write(s.getBytes());
+			}
+			out.close();
+			result += getResultByCids(cids, dbno, starttime);
 			out = new FileOutputStream(file);
+			out.write(result.getBytes());
 		} catch (IOException e) {
 			e.printStackTrace();
-		}
-		double count = -1;
-		String size = "-1";
-		String oldsize = "-1";
-		String head = "cid\tsize\toldsize\tkwcount\n";
-		try {
-			out.write(head.getBytes());
-		} catch (IOException e3) {
-			e3.printStackTrace();
-		}
-		for (Integer cid : cids) {
-			try {
-				size = sizeDao.getSizeByCid(dbno, cid);
-				oldsize = sizeDao.getOldSizeByCid(dbno, cid);
-				count = sizeDao.getKwCountByCidStartTime(dbno, cid, starttime);
-				String s = cid + "\t" + size + "\t" + oldsize + "\t" + count + "\n";
-				out.write(s.getBytes());
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
 		}
 		if (out != null) {
 			try {
@@ -120,9 +157,32 @@ public class App {
 				e.printStackTrace();
 			}
 		}
+		if (bf != null) {
+			try {
+				bf.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 		System.out.println("db" + dbno + "  over!");
 		Dao.getDao().close(dbno);
 		System.out.println(new Date().toString());
+	}
+
+	private static String getResultByCids(List<Integer> cids, int dbno, String starttime) {
+		String ret = "";
+		double count = -1;
+		String size = "-1";
+		// String oldsize = "-1";
+		SizeDao sizeDao = new SizeDao();
+		for (Integer cid : cids) {
+			size = sizeDao.getSizeByCid(dbno, cid);
+			// oldsize = sizeDao.getOldSizeByCid(dbno, cid);
+			count = sizeDao.getKwCountByCidStartTime(dbno, cid, starttime);
+			String s = cid + "\t" + size + "\t" + count + "\n";
+			ret += s;
+		}
+		return ret;
 	}
 
 }
